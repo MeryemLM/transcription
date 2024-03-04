c4c7fab6bb0c609f9d5c5b7ae3cdc3c26d55b751
MeryemLM <121564877+MeryemLM@users.noreply.github.com>
2024-03-04 11:26:31 +0100

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -3,25 +3,21 @@ import assemblyai as aai
 from transformers import pipeline
 import tempfile
 import openai
-import os
 
 st.set_page_config(layout="wide")
+
 
 def transcribe_audio(audio_path):
     # Configuration de l'API AssemblyAI
     aai.settings.api_key = "146c7980fa5a4b6c872033d97234500b"
- 
+
     # Création d'un transcriber
     transcriber = aai.Transcriber()
     # Configuration de la transcription
     config = aai.TranscriptionConfig(language_code="fr", speaker_labels=True, speakers_expected=2)
-    if os.path.exists(audio_path):
-        # Le fichier existe, procédez à la transcription
-        transcript = transcriber.transcribe(audio_path, config)
     # Transcription de l'audio
-    #transcript = transcriber.transcribe(audio_path, config)
+    transcript = transcriber.transcribe(audio_path, config)
     return transcript
-
 
 sentiment_analysis = pipeline(
   "sentiment-analysis",
