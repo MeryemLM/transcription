9d68cd8c76bef27f9a189d9272eb8c62058e6ef4
MeryemLM <121564877+MeryemLM@users.noreply.github.com>
2024-03-05 22:16:44 +0100

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -81,14 +81,14 @@ def main():
         
         # Ajouter un composant pour uploader un fichier audio
     uploaded_file = st.file_uploader("Téléverser un fichier audio", type=["mp3", "wav"])
-    #audio_data = io.BytesIO(uploaded_file.read())
-    #st.audio(audio_data, format='audio/wav')
+    
     # Lire le fichier audio téléversé et le jouer
     
 
         # Créer une rangée pour les boutons "Transcription" et "Emotion"
     button_col1, button_col2, button_col3 = st.columns(3)
-
+    audio_data = io.BytesIO(uploaded_file.read())
+    st.audio(audio_data, format='audio/wav')
         # Vérifier si un fichier a été uploadé
     if uploaded_file is not None:
         
@@ -96,8 +96,7 @@ def main():
         with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
             tmp_file.write(uploaded_file.read())
             audio_path = tmp_file.name
-            audio_data = io.BytesIO(uploaded_file.read())
-            st.audio(audio_data, format='audio/wav')
+            
             # Boutons pour la transcription et l'analyse de l'émotion
         if button_col1.button("Transcription") :
 
